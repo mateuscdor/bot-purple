accabe7f69b9d9992a407b4ba12fb413c4d0af1d
gkp1 <63517877+gkp1@users.noreply.github.com>
2022-01-27 21:54:47 -0300

    Delete dist directory

--- dist/between.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.between = void 0;
-//==============================================================================
-function between(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-exports.between = between;
--- dist/tsconfig.js
+++
@@ -1,4 +0,0 @@
-{
-    "esModuleInterop";
-    true;
-}
